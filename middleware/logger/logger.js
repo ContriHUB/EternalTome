6293e6ff2603c34009cc900e81f49a324ca45602
lokesh-wagh <lokeshwagh004@gmail.com>
2025-04-19 14:42:10 +0530

    added a logger function

---
+++ middleware/logger/logger.js
@@ -0,0 +1,18 @@
+// logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(
+      ({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`
+    )
+  ),
+  transports: [
+    new transports.Console(),                         // consoleg print
+    new transports.File({ filename: 'security.log' }) // Save to file
+  ],
+});
+
+module.exports = logger;
